efa1417c72b641ba96c08e4a87d0ac65632ced21
CrashTV1334 <iit2019033@iiita.ac.in>
2021-03-24 16:35:08 +0530

    Landing Page Done

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static("public"));
+
+var port = process.env.PORT || 3000;
+
+app.get("/", function (req, res) {
+    res.render("index.ejs");
+});
+
+app.get("/veteran", function (req, res) {
+    res.send("VETREE");
+});
+
+app.get("/covid", function (req, res) {
+    res.send("COVID");
+});
+
+app.listen(port, function () {
+    console.log("server started on port 3000");
+});
