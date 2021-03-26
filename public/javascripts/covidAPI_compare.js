93e84a1d3dcdf4f84d98f6c9443bd9997d742ff6
CrashTV1334 <iit2019033@iiita.ac.in>
2021-03-26 16:48:46 +0530

    Compare States Basics Done

--- public/javascripts/covidAPI_compare.js
+++ public/javascripts/covidAPI_compare.js
@@ -10,20 +10,26 @@ xmlhttp.onreadystatechange = function () {
 xmlhttp.open("GET", url, true);
 xmlhttp.send();
 
-var scannedAC = [];
-var scannedRC = [];
-var scannedDTH = [];
-var scannedLabels = [];
 var ttc, ttr, ttd, tta, rr, dr;
+var dtc, dtr, dtd, state1, state2;
+var data1 = [];
+var data2 = [];
+var data11 = [];
+var data22 = [];
+var data111 = [];
+var data222 = [];
+var label = ["Total Confirmed", "Total Recovered", "Total Deceased", "Total Active"];
+var label1 = ["Delta Confirmed", "Delta Recovered", "Delta Deceased"];
 
 var stateMap1 = new Map();
 stateMap1.set("Total", 0);
 var stateMap2 = new Map();
 stateMap2.set("Total", 0);
-var label = ["Total Confirmed", "Total Recovered", "Total Deceased", "Total Active"]
-var label1 = ["Recovery Rate", "Death Rate"];
 
 function myFunction(arr) {
+
+
+
     var stateArr = arr.statewise;
     var stateSel1 = document.querySelector("#stateSel1");
     stateSel1.length = 1;
@@ -34,10 +40,22 @@ function myFunction(arr) {
     ttr = stateArr[0].recovered;
     ttd = stateArr[0].deaths;
     tta = stateArr[0].active;
+    dtc = stateArr[0].deltaconfirmed;
+    dtd = stateArr[0].deltadeaths;
+    dtr = stateArr[0].deltarecovered;
     rr = ((ttr / ttc) * 100).toFixed(2);
     dr = ((ttd / ttc) * 100).toFixed(2);
-    setParameters(ttc, ttr, ttd, tta, rr, dr, '1');
-    setParameters(ttc, ttr, ttd, tta, rr, dr, '2');
+
+    data1 = [ttc, ttr, ttd, tta];
+    data2 = [ttc, ttr, ttd, tta];
+    data11 = [dtc, dtr, dtd];
+    data22 = [dtc, dtr, dtd];
+    data111 = [rr, dr];
+    data222 = [rr, dr];
+
+
+    setParameters(ttc, ttr, ttd, tta, rr, dr, '1', 'Total', 'Total');
+    setParameters(ttc, ttr, ttd, tta, rr, dr, '2', 'Total', 'Total');
     for (var i = 1; i < stateArr.length; i++) {
         stateSel1.options[stateSel1.options.length] = new Option(stateArr[i].state, stateArr[i].state);
         stateMap1.set(stateArr[i].state, i);
@@ -55,13 +73,21 @@ function myFunction(arr) {
             var tr = stateArr[indx].recovered;
             var td = stateArr[indx].deaths;
             var ta = stateArr[indx].active;
+            state1 = stateArr[indx].state;
+            var state = stateArr[indx].state;
             ttc = tc;
             ttr = tr;
             ttd = td;
             tta = ta;
+            dtc = stateArr[indx].deltaconfirmed;
+            dtd = stateArr[indx].deltadeaths;
+            dtr = stateArr[indx].deltarecovered;
             rr = ((ttr / ttc) * 100).toFixed(2);
             dr = ((ttd / ttc) * 100).toFixed(2);
-            setParameters(ttc, ttr, ttd, tta, rr, dr, '1');
+            data1 = [ttc, ttr, ttd, tta];
+            data11 = [dtc, dtr, dtd];
+            data111 = [rr, dr];
+            setParameters(ttc, ttr, ttd, tta, rr, dr, '1', state1, state2);
 
         } catch (error) {
             try {
@@ -69,21 +95,34 @@ function myFunction(arr) {
                 var tr = stateArr[0].recovered;
                 var td = stateArr[0].deaths;
                 var ta = stateArr[0].active;
+                state1 = stateArr[0].state;
                 ttc = tc;
                 ttr = tr;
                 ttd = td;
                 tta = ta;
+                dtc = stateArr[0].deltaconfirmed;
+                dtd = stateArr[0].deltadeaths;
+                dtr = stateArr[0].deltarecovered;
                 rr = ((ttr / ttc) * 100).toFixed(2);
                 dr = ((ttd / ttc) * 100).toFixed(2);
-                setParameters(ttc, ttr, ttd, tta, rr, dr, '1');
+                data1 = [ttc, ttr, ttd, tta];
+                data11 = [dtc, dtr, dtd];
+                data111 = [rr, dr];
+                setParameters(ttc, ttr, ttd, tta, rr, dr, '1', state1, state2);
             } catch (error) {
                 ttc = 0;
                 ttr = 0;
                 ttd = 0;
                 tta = 0;
+                dtc = 0;
+                dtd = 0;
+                dtr = 0;
                 rr = ((ttr / ttc) * 100).toFixed(2);
                 dr = ((ttd / ttc) * 100).toFixed(2);
-                setParameters(ttc, ttr, ttd, tta, rr, dr, '1');
+                data1 = [ttc, ttr, ttd, tta];
+                data11 = [dtc, dtr, dtd];
+                data111 = [rr, dr];
+                setParameters(ttc, ttr, ttd, tta, rr, dr, '1', 'state', 'state');
             }
 
         }
@@ -96,13 +135,20 @@ function myFunction(arr) {
                 var tr = stateArr[indx].recovered;
                 var td = stateArr[indx].deaths;
                 var ta = stateArr[indx].active;
+                state2 = stateArr[indx].state;
                 ttc = tc;
                 ttr = tr;
                 ttd = td;
                 tta = ta;
+                dtc = stateArr[indx].deltaconfirmed;
+                dtd = stateArr[indx].deltadeaths;
+                dtr = stateArr[indx].deltarecovered;
                 rr = ((ttr / ttc) * 100).toFixed(2);
                 dr = ((ttd / ttc) * 100).toFixed(2);
-                setParameters(ttc, ttr, ttd, tta, rr, dr, '2');
+                data2 = [ttc, ttr, ttd, tta];
+                data22 = [dtc, dtr, dtd];
+                data222 = [rr, dr];
+                setParameters(ttc, ttr, ttd, tta, rr, dr, '2', state1, state2);
 
             } catch (error) {
                 try {
@@ -110,21 +156,35 @@ function myFunction(arr) {
                     var tr = stateArr[0].recovered;
                     var td = stateArr[0].deaths;
                     var ta = stateArr[0].active;
+                    state2 = stateArr[0].state;
                     ttc = tc;
                     ttr = tr;
                     ttd = td;
                     tta = ta;
+                    dtc = stateArr[0].deltaconfirmed;
+                    dtd = stateArr[0].deltadeaths;
+                    dtr = stateArr[0].deltarecovered;
                     rr = ((ttr / ttc) * 100).toFixed(2);
                     dr = ((ttd / ttc) * 100).toFixed(2);
-                    setParameters(ttc, ttr, ttd, tta, rr, dr, '2');
+                    data2 = [ttc, ttr, ttd, tta];
+                    data22 = [dtc, dtr, dtd];
+                    data222 = [rr, dr];
+                    setParameters(ttc, ttr, ttd, tta, rr, dr, '2', state1, state2);
                 } catch (error) {
                     ttc = 0;
                     ttr = 0;
                     ttd = 0;
                     tta = 0;
+                    dtc = 0;
+                    dtd = 0;
+                    dtr = 0;
+                    var state = stateArr[0].state;
                     rr = ((ttr / ttc) * 100).toFixed(2);
                     dr = ((ttd / ttc) * 100).toFixed(2);
-                    setParameters(ttc, ttr, ttd, tta, rr, dr, '2');
+                    data2 = [ttc, ttr, ttd, tta];
+                    data22 = [dtc, dtr, dtd];
+                    data222 = [rr, dr];
+                    setParameters(ttc, ttr, ttd, tta, rr, dr, '2', 'state', 'state');
                 }
 
             }
@@ -133,7 +193,7 @@ function myFunction(arr) {
     }
 }
 
-function setParameters(ttc, ttr, ttd, tta, rr, dr, offset) {
+function setParameters(ttc, ttr, ttd, tta, rr, dr, offset, state1, state2) {
     var TC = document.querySelector("#confirmCs" + offset);
     var RC = document.querySelector("#recoveries" + offset);
     var DC = document.querySelector("#deaths" + offset);
@@ -146,64 +206,69 @@ function setParameters(ttc, ttr, ttd, tta, rr, dr, offset) {
     AC.textContent = tta;
     DR.textContent = dr + " %";
     RR.textContent = rr + " %";
-    // createGraph([ttc, ttr, ttd, tta], label);
-    // createBarGraph([rr, dr], label1);
+    createRadarGraph(data1, data2, label, 'radarChart', '#totalChart', 'Overall');
+    createRadarGraph(data11, data22, label1, 'RradarChart', '#deltaChart', 'Delta');
+    createBarGraph(data111, 'bbarChart', '#rates1', state1);
+    createBarGraph(data222, 'BbarChart', '#rates2', state2);
 }
 
-var pt = 0;
-
-function createGraph(dat, label) {
-    var variable = '<canvas id="pieChart">< /canvas>';
-    $('#pieChart').remove();
-    $('#totalChart').append(variable);
-    var ctx = document.getElementById('pieChart').getContext('2d');
+function createRadarGraph(data1, data2, Llabel, id1, id2, heading) {
+    var variable = '<canvas id="' + id1 + '"></canvas>';
+    $('#' + id1).remove();
+    $(id2).append(variable);
+    var ctx = document.getElementById(id1).getContext('2d');
     var myChart = new Chart(ctx, {
-        type: 'doughnut',
+        type: 'radar',
         data: {
             datasets: [{
-                data: dat,
-                borderWidth: 1,
+                data: data1,
                 backgroundColor: [
-                    'orange',
-                    'green',
-                    'red',
-                    'yellow',
+                    'rgba(255,0,0,0.3)',
+                ],
+            }, {
+                data: data2,
+                backgroundColor: [
+                    'rgba(0,0,255,0.3)',
                 ],
             }],
-            labels: label,
+            labels: Llabel,
         },
         options: {
-            legend: {
+            title: {
                 display: true,
+                text: heading,
             },
+            legend: {
+                display: false,
+            }
         }
 
     });
 
 }
 
-function createBarGraph(dat1, label1) {
-    var variable = '<canvas id="barChart">< /canvas>';
-    $('#barChart').remove();
-    $('#rateChart').append(variable);
-    var ctx = document.getElementById('barChart').getContext('2d');
+function createBarGraph(dat, id1, id2, state) {
+    var variable = '<canvas id="' + id1 + '"></canvas>';
+    $('#' + id1).remove();
+    $(id2).append(variable);
+    var ctx = document.getElementById(id1).getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
         data: {
             datasets: [{
-                data: dat1,
+                data: dat,
                 borderWidth: 1,
                 backgroundColor: [
-                    'green',
-                    'red',
+                    'rgba(0,255,0,0.3)',
+                    'rgba(255,0,0,0.3)',
                 ],
             }],
-            labels: label1,
+            labels: ['Recovery Rate', 'Death Rate'],
         },
         options: {
             title: {
                 display: true,
-                text: 'Rates',
+                text: state,
             },
             legend: {
                 display: false,
